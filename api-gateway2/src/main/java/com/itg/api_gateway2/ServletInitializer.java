ff74d4eb7407d0f0ed9bde86e3b43dde7071002e
ibrahimgokyar1 <115937578+ibrahimgokyar1@users.noreply.github.com>
2024-09-04 14:09:02 +0300

    init 2

---
+++ api-gateway2/src/main/java/com/itg/api_gateway2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.itg.api_gateway2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiGateway2Application.class);
+	}
+
+}
---
+++ product-service/src/test/java/com/itg/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itg.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
